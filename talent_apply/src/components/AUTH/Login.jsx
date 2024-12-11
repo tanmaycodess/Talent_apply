2e4da2ef9248611b3ed5f759a573f5e532a509bc
tanmaycodess <97839983+tanmaycodess@users.noreply.github.com>
2024-12-11 23:07:01 +0530

    Update Login.jsx

--- talent_apply/src/components/AUTH/Login.jsx
+++ talent_apply/src/components/AUTH/Login.jsx
@@ -23,7 +23,7 @@ const Auth = () => {
         setError('');
 
         try {
-            const response = await fetch('https://talentapply-1s9izbs7.b4a.run/api/login', {
+            const response = await fetch('https://talent-apply-backend.vercel.app/api/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(loginData),
